98d433992826ee74f602c3cfab03bb23e77a3d8b
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-06-29 16:21:19 -0400

    Worked on fetch

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much.";
+const token = "41e3a63624cb661af97a26ca9fa5d2f536340d5a";
+
+// fetch('https://api.github.com/repos/jquery/jquery/commits', {
+//   headers:{Authorization: `token ${token}`}
+// })
+//   .then(res => res.json())
+//   .then(json => console.log(json));
+
+
+  fetch('https://api.github.com/user/repos', {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
